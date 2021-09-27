e54b21f0067ed3fbb9ce7f7c13c3cbcf6dc4a358
Hugo <hugo.frias.isc@gmail.com>
2021-09-27 18:23:41 -0500

    Primer commit

---
+++ src/js/componentes.js
@@ -0,0 +1,16 @@
+import '../css/componentes.css'
+/* import webpacklogo from '../assets/imgs/webpack-logo.png' */
+
+export const saludar = ( nombre = 'sin nombre') => {
+
+    console.log('Creando etiqueta h1');
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola, ${ nombre }!!!`;
+
+    document.body.append( h1 );
+
+/*     const img = document.createElement('img');
+    img.src = webpacklogo;
+    document.body.append( img ); */
+}
